23950f2c5eb670f16c50db60e624be3638fe05d2
Natcha Nanthawisit <natcha.nanthawisit@nav.no>
2022-11-25 09:30:15 +0100

    fixing merge conflicts

--- src/lib/security/oboproviders/index.ts
+++ src/lib/security/oboproviders/index.ts
@@ -1,14 +1,12 @@
-import {AuthProvider} from "../providers/AuthProvider";
+import { IAuthProvider } from "../providers/AuthProvider";
 import tokenx from "./tokenx";
 
-export function oboToken(provider: AuthProvider, subject_token: string) {
+export function oboToken(provider: IAuthProvider, subject_token: string) {
   switch (provider.name) {
     case "idporten":
-      return async (audience: string) =>
-          tokenx.exchangeToken()(subject_token, audience);
+      return async (audience: string) => tokenx.exchangeToken()(subject_token, audience);
     case "mock":
-      return async (audience: string) =>
-          tokenx.exchangeToken()(subject_token, audience);
+      return async (audience: string) => tokenx.exchangeToken()(subject_token, audience);
     default:
       throw new Error("Missing token issuer for this provider");
   }
