7ab4d702d21ab9cb837819c195ed5b10ba23be42
Ugur Cenar <cenar93@gmail.com>
2022-11-26 17:00:53 +0100

    Logging and redis caching of OBO token

---
+++ src/lib/cache/cache.ts
@@ -0,0 +1,15 @@
+import {createRedisInstance} from "./redis";
+import {getLocalCache} from "./localcache";
+import {TCache} from "./types";
+import environment from "../../environment";
+
+async function getCache() {
+  if (environment.redis.enabled){
+    return createRedisInstance();
+  }
+
+  return getLocalCache()
+}
+
+const cache: TCache = await getCache()
+export default cache
