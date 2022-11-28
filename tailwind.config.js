99e4aa7850dfc82eb928543a9fd544a84f77bbc5
Natcha Nanthawisit <natcha.nanthawisit@nav.no>
2022-11-28 10:40:50 +0100

    adding views folder to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
     presets: [require("@navikt/ds-tailwind")],
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/views/**/*.{js,ts,jsx,tsx}",
         "./src/components/**/*.{js,ts,jsx,tsx}",
       ],
       "theme": {
