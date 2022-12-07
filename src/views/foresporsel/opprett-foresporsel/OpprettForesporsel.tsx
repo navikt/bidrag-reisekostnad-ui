0ae3436879e521511398d465307eeaf28a7ba936
Natcha Nanthawisit <natcha.nanthawisit@nav.no>
2022-12-07 11:48:57 +0100

    show not be able to create forespørsel if there is no available barn

--- src/views/foresporsel/opprett-foresporsel/OpprettForesporsel.tsx
+++ src/views/foresporsel/opprett-foresporsel/OpprettForesporsel.tsx
@@ -91,66 +91,73 @@ export default function OpprettForesporsel() {
             Fordeling av reisekostnader
           </Heading>
           {allBarn.length === 0 && <Alert variant="info">Du har ingen tilgjengelig barn</Alert>}
-          <GuidePanel>
-            <p>
-              Her kan du sende inn en forespørsel om NAV kan beregne reisekostnader ved samvær med
-              barn.
-            </p>
-            <p>
-              Normalt skal dere som foreldre selv avtale fordelingen på den måten dere synes er
-              best. Det vanlige er at reisekostnadene fordeles forholdsmessig mellom dere, etter
-              størrelsen på inntekten deres. For at NAV skal beregne for dere, må det foreligge
-              såkalte særlige grunner. Dette kan være hvis dere er uenige i fordelingen, samtidig
-              som det er store inntektsforskjeller og store reisekostnader.
-            </p>
-            <b>Samtykke fra den andre forelderen</b>
-            <p>
-              Når barnet er under 15 år, må begge foreldre være enige i at NAV skal beregne
-              fordelingen for dere. Vi innhenter derfor et samtykke fra den andre forelderen. Hvis
-              den andre forelderen ikke vil samtykke, må NAV avvise forespørselen om beregning.
-            </p>
-            <p>
-              Når barnet er 15 år, eller eldre, trengs det ikke samtykke fra den andre forelderen.
-              Da blir forespørselen automatisk sendt videre til behandling.
-            </p>
-          </GuidePanel>
-          {userInformation && userInformation.harSkjulteFamilieenheterMedDiskresjon && (
-            <Alert variant="info">TODO: personen har skjulte familieenheter med diskresjon</Alert>
-          )}
-          <BarnContainer
-            allBarn={allBarn}
-            foundPersonOver15={foundPersonOver15}
-            foundPersonCouldBe15In30Days={foundPersonCouldBe15In30Days}
-            onSelectBarn={onSelectBarn}
-            showError={showBarnError}
-          />
-          <OppsummeringContainer
-            checked={samtykke}
-            updateChecked={onSamtykke}
-            showError={showSamtykkeError}
-          />
-          <div className="flex gap-5">
-            <Button onClick={onSubmit} loading={submitting}>
-              SEND INN
-            </Button>
-            <Button
-              type="button"
-              variant="secondary"
-              onClick={() => setOpen((current) => !current)}
-            >
-              AVBRYT
-            </Button>
-          </div>
-          <ConfirmModal
-            open={open}
-            header="Vil du avbryte forerspørselen?"
-            content="Avbryter du nå, skal forerspørselen slettes og 
+          {allBarn.length > 0 && (
+            <>
+              <GuidePanel>
+                <p>
+                  Her kan du sende inn en forespørsel om NAV kan beregne reisekostnader ved samvær
+                  med barn.
+                </p>
+                <p>
+                  Normalt skal dere som foreldre selv avtale fordelingen på den måten dere synes er
+                  best. Det vanlige er at reisekostnadene fordeles forholdsmessig mellom dere, etter
+                  størrelsen på inntekten deres. For at NAV skal beregne for dere, må det foreligge
+                  såkalte særlige grunner. Dette kan være hvis dere er uenige i fordelingen,
+                  samtidig som det er store inntektsforskjeller og store reisekostnader.
+                </p>
+                <b>Samtykke fra den andre forelderen</b>
+                <p>
+                  Når barnet er under 15 år, må begge foreldre være enige i at NAV skal beregne
+                  fordelingen for dere. Vi innhenter derfor et samtykke fra den andre forelderen.
+                  Hvis den andre forelderen ikke vil samtykke, må NAV avvise forespørselen om
+                  beregning.
+                </p>
+                <p>
+                  Når barnet er 15 år, eller eldre, trengs det ikke samtykke fra den andre
+                  forelderen. Da blir forespørselen automatisk sendt videre til behandling.
+                </p>
+              </GuidePanel>
+              {userInformation && userInformation.harSkjulteFamilieenheterMedDiskresjon && (
+                <Alert variant="info">
+                  TODO: personen har skjulte familieenheter med diskresjon
+                </Alert>
+              )}
+              <BarnContainer
+                allBarn={allBarn}
+                foundPersonOver15={foundPersonOver15}
+                foundPersonCouldBe15In30Days={foundPersonCouldBe15In30Days}
+                onSelectBarn={onSelectBarn}
+                showError={showBarnError}
+              />
+              <OppsummeringContainer
+                checked={samtykke}
+                updateChecked={onSamtykke}
+                showError={showSamtykkeError}
+              />
+              <div className="flex gap-5">
+                <Button onClick={onSubmit} loading={submitting}>
+                  SEND INN
+                </Button>
+                <Button
+                  type="button"
+                  variant="secondary"
+                  onClick={() => setOpen((current) => !current)}
+                >
+                  AVBRYT
+                </Button>
+              </div>
+              <ConfirmModal
+                open={open}
+                header="Vil du avbryte forerspørselen?"
+                content="Avbryter du nå, skal forerspørselen slettes og 
             du må starte på nytt."
-            submitText="Tilbake til søknaden"
-            onSubmit={() => setOpen(false)}
-            onCancel={() => router.push("/")}
-            onClose={() => setOpen(false)}
-          />
+                submitText="Tilbake til søknaden"
+                onSubmit={() => setOpen(false)}
+                onCancel={() => router.push("/")}
+                onClose={() => setOpen(false)}
+              />
+            </>
+          )}
         </>
       )}
     </div>
