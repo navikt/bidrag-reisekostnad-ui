536511e23a7d2ac611750ed5ef46253dc54fc51d
Natcha Nanthawisit <natcha.nanthawisit@nav.no>
2022-12-07 10:55:39 +0100

    person with no barn should not be able to use this app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,37 @@
 import Overview from "../views/overview/Overview";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useReisekostnad } from "../context/reisekostnadContext";
 import { IBrukerinformasjon } from "../types/foresporsel";
 import Spinner from "../components/spinner/spinner/spinner";
 import useSWR from "swr";
 import { generateAndStoreCorrelationIdAsCookie } from "../lib/logging/types";
 import { fetcher } from "../utils/apiUtils";
+import { Alert } from "@navikt/ds-react";
 
 export default function Home() {
   const { data } = useSWR<IBrukerinformasjon>("/api/brukerinformasjon", fetcher);
   const { updateUserInformation } = useReisekostnad();
+  const [hasNoBarn, setHasNoBarn] = useState<boolean>(false);
 
   useEffect(generateAndStoreCorrelationIdAsCookie, []);
 
   useEffect(() => {
     if (data) {
+      const hasBarn =
+        data.barnMinstFemtenÅr.length === 0 &&
+        data.motparterMedFellesBarnUnderFemtenÅr.flatMap((person) => person.fellesBarnUnder15År)
+          .length === 0;
+
       updateUserInformation(data);
+      setHasNoBarn(hasBarn);
     }
   }, [data]);
 
   if (!data) return <Spinner />;
+
+  if (hasNoBarn) {
+    return <Alert variant="info">Kan ikke bruke løsningen fordi du ikke har barn</Alert>;
+  }
 
   return <Overview />;
 }
