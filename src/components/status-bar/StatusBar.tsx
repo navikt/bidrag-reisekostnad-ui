f1b40361869d9a27af7f1df092aa5a08338e67ee
Natcha Nanthawisit <natcha.nanthawisit@nav.no>
2022-12-08 09:12:28 +0100

    using i17next on status

--- src/components/status-bar/StatusBar.tsx
+++ src/components/status-bar/StatusBar.tsx
@@ -1,11 +1,14 @@
 import { Tag } from "@navikt/ds-react";
 import { ForesporselStatus } from "../../enum/foresporsel-status";
+import { useTranslation } from "next-i18next";
 
 interface IStatusBar {
   status: ForesporselStatus;
 }
 
 export default function StatusBar({ status }: IStatusBar) {
+  const { t: translate } = useTranslation();
+
   return (
     <>
       {(() => {
@@ -13,25 +16,25 @@ export default function StatusBar({ status }: IStatusBar) {
           case ForesporselStatus.VENTER_PAA_SAMTYKKE:
             return (
               <Tag variant="warning" size="small" className="mt-3">
-                {status}
+                {translate("status.venter_paa_samtykke")}
               </Tag>
             );
           case ForesporselStatus.TREKKET_TILBAKE:
             return (
               <Tag variant="neutral" size="small" className="mt-3">
-                {status}
+                {translate("status.trekket_tilbake")}
               </Tag>
             );
           case ForesporselStatus.VENTER_PAA_OVERFORING:
             return (
               <Tag variant="alt2" size="small" className="mt-3">
-                {status}
+                {translate("status.venter_paa_overforing")}
               </Tag>
             );
           default:
             return (
               <Tag variant="success" size="small" className="mt-3">
-                {status}
+                {translate("status.under_behandling")}
               </Tag>
             );
         }
