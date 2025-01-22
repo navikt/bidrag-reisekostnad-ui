2d382e370bdcae736bbb9894371e714395effaf6
Tatjana Andersen <43849660+tatjanaand@users.noreply.github.com>
2025-01-22 16:28:43 +0100

    Setup msw hooks and next-i18next in vitest

--- setup-file.ts
+++ setup-file.ts
@@ -1,1 +1,37 @@
 import '@testing-library/jest-dom/vitest';
+
+import { beforeAll, afterEach, afterAll, vi } from 'vitest'
+import { server } from "./src/__mocks__/server";
+
+// Start the server before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+
+// Clean up after the tests are finished
+afterAll(() => server.close());
+
+// Reset any request handlers that are declared as a part of our tests
+// (i.e. for testing one-time error scenarios)
+afterEach(() => server.resetHandlers());
+
+vi.mock("next-i18next", () => ({
+  useTranslation: () => {
+    return {
+      t: (str: string) => {
+        if (str.includes("accordion")) {
+          return [
+            {
+              header: "",
+              content: "",
+            },
+          ];
+        } else {
+          return str;
+        }
+      },
+      i18n: {
+        language: "nb",
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+}));
