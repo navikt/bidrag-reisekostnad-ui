e7ae10c20b1209ecf033c7e5c116edeabfcb04ed
Tatjana Andersen <43849660+tatjanaand@users.noreply.github.com>
2025-03-07 10:19:15 +0100

    ESLint v8.x EOL, upgrade from to 8.28.0 to 9.21.0   * new flat file configuration craeted with     npx  @eslint/migrate-config .eslintrc.json

---
+++ eslint.config.mjs
@@ -0,0 +1,79 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import jsxA11Y from "eslint-plugin-jsx-a11y";
+import jestDom from "eslint-plugin-jest-dom";
+import testingLibrary from "eslint-plugin-testing-library";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...fixupConfigRules(compat.extends(
+    "eslint:recommended",
+    "next/core-web-vitals",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+)), {
+    plugins: {
+        react: fixupPluginRules(react),
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+        prettier: fixupPluginRules(prettier),
+        "jsx-a11y": fixupPluginRules(jsxA11Y),
+        "jest-dom": jestDom,
+        "testing-library": testingLibrary,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 12,
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/ban-ts-ignore": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+
+        "@typescript-eslint/naming-convention": ["warn", {
+            selector: "interface",
+            format: ["PascalCase"],
+
+            custom: {
+                regex: "^I[A-Z]",
+                match: true,
+            },
+        }],
+
+        "react-hooks/exhaustive-deps": "off",
+        "react/prop-types": "off",
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+        "no-console": "warn",
+        "prettier/prettier": "warn",
+    },
+}];
