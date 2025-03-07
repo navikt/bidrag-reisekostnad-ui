6ef78c2c2c1d82d6783c72df17a7c79eb64f11e8
Tatjana Andersen <43849660+tatjanaand@users.noreply.github.com>
2025-03-07 17:37:30 +0100

    A `require()` style import is forbidden @typescript-eslint/no-require-imports

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
