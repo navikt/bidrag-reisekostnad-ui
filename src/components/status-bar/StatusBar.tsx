adfb3ce7605e450642104c11f1ec18793235ce3f
Tatjana Andersen <43849660+tatjanaand@users.noreply.github.com>
2026-01-23 10:44:49 +0100

    Run aksel codemod v8-tag-variant

--- src/components/status-bar/StatusBar.tsx
+++ src/components/status-bar/StatusBar.tsx
@@ -15,25 +15,25 @@ export default function StatusBar({ status }: IStatusBar) {
                 switch (status) {
                     case ForesporselStatus.VENTER_PAA_SAMTYKKE_FRA_DEG:
                         return (
-                            <Tag variant="warning" size="small" className="mt-3">
+                            <Tag data-color="warning" variant="outline" size="small" className="mt-3">
                                 {translate('status.venter_paa_samtykke_fra_deg')}
                             </Tag>
                         );
                     case ForesporselStatus.VENTER_PAA_SAMTYKKE_FRA_DEN_ANDRE_FORELDEREN:
                         return (
-                            <Tag variant="warning" size="small" className="mt-3">
+                            <Tag data-color="warning" variant="outline" size="small" className="mt-3">
                                 {translate('status.venter_paa_samtykke_fra_den_andre_forelderen')}
                             </Tag>
                         );
                     case ForesporselStatus.KANSELLERT:
                         return (
-                            <Tag variant="neutral" size="small" className="mt-3">
+                            <Tag data-color="neutral" variant="outline" size="small" className="mt-3">
                                 {translate('status.kansellert')}
                             </Tag>
                         );
                     default:
                         return (
-                            <Tag variant="success" size="small" className="mt-3">
+                            <Tag data-color="success" variant="outline" size="small" className="mt-3">
                                 {translate('status.under_behandling')}
                             </Tag>
                         );
