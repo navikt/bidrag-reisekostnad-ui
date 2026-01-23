b9c2a1bc74e080384627e0747fb88d9516a41a31
Tatjana Andersen <43849660+tatjanaand@users.noreply.github.com>
2026-01-23 10:54:30 +0100

    Run prettier on StatusBar.tsx

--- src/components/status-bar/StatusBar.tsx
+++ src/components/status-bar/StatusBar.tsx
@@ -15,25 +15,45 @@ export default function StatusBar({ status }: IStatusBar) {
                 switch (status) {
                     case ForesporselStatus.VENTER_PAA_SAMTYKKE_FRA_DEG:
                         return (
-                            <Tag data-color="warning" variant="outline" size="small" className="mt-3">
+                            <Tag
+                                data-color="warning"
+                                variant="outline"
+                                size="small"
+                                className="mt-3"
+                            >
                                 {translate('status.venter_paa_samtykke_fra_deg')}
                             </Tag>
                         );
                     case ForesporselStatus.VENTER_PAA_SAMTYKKE_FRA_DEN_ANDRE_FORELDEREN:
                         return (
-                            <Tag data-color="warning" variant="outline" size="small" className="mt-3">
+                            <Tag
+                                data-color="warning"
+                                variant="outline"
+                                size="small"
+                                className="mt-3"
+                            >
                                 {translate('status.venter_paa_samtykke_fra_den_andre_forelderen')}
                             </Tag>
                         );
                     case ForesporselStatus.KANSELLERT:
                         return (
-                            <Tag data-color="neutral" variant="outline" size="small" className="mt-3">
+                            <Tag
+                                data-color="neutral"
+                                variant="outline"
+                                size="small"
+                                className="mt-3"
+                            >
                                 {translate('status.kansellert')}
                             </Tag>
                         );
                     default:
                         return (
-                            <Tag data-color="success" variant="outline" size="small" className="mt-3">
+                            <Tag
+                                data-color="success"
+                                variant="outline"
+                                size="small"
+                                className="mt-3"
+                            >
                                 {translate('status.under_behandling')}
                             </Tag>
                         );
