9009097f26623a2a2938b249b9306d60089c1752
tatjanaand <43849660+tatjanaand@users.noreply.github.com>
2026-04-16 14:59:04 +0200

    Upgrade packages (#944)
    * Previously autoupgraded
    
    * Upgrade vite and vitest
    
    * Upgrade @vitejs/plugin-react to support vite 8
    
    * Remove vite-tsconfig-paths
      - Vite 8 supports tsconfig path resolution natively via the
        resolve.tsconfigPaths option
      - Add baseUrl to tsconfig.json to resolve non‑relative imports
        starting from the project root
      - Vite relies on TypeScript’s interpretation of paths
    
    * Fresh install, some missing packages in lock file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
+    plugins: [react()],
     test: {
         environment: 'jsdom',
         setupFiles: './setup.vitest.ts',
         globals: true,
+    },
+    resolve: {
+        tsconfigPaths: true,
     },
 });
